57ca1deafcff9e0f2a8097c2c862324ce444d0b8
Emily Wairimu <emily.wairimu@student.moringaschool.com>
2023-03-28 16:11:28 -0500

    completed test

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
+function reverse(word) {
+  const wordArray = word.split("");
+  const reversedWordArray = wordArray.reverse();
+  const reversedWord = reversedWordArray.join("");
+  return reversedWord;
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverse(word);
+  // check if word is same as reversed word
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+
 */
 
 /*
   Add written explanation of your solution here
+  a palindrome is a word that is same when written in reverse
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +36,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("on"));
 }
 
 module.exports = isPalindrome;
